e692d1ee435bcfbed10367debad0975a5f974653
Pallavi K R <pallavi.kr@mistral.in>
2022-11-15 16:37:12 +0530

    Added multiple methods in testbase. Added python file to call methods.

---
+++ Script/Module_execute.py
@@ -0,0 +1,23 @@
+from helper.test_base import TestBase
+
+list_for_sum = [34, 56, 93.89, 130, 4]
+list_for_difference = [56, 98.98765]
+list_for_product = [67, 3.4, 23, 5]
+list_for_quotient = [678, 12]
+list_for_random = [9, 89]
+string = "Current System: Windows."
+
+
+class Module(TestBase):
+    def call_functions(self):
+        self.add_numbers(list_for_sum)
+        self.subtract_numbers(list_for_difference)
+        self.multiply_numbers(list_for_product)
+        self.divide_numbers(list_for_quotient)
+        self.generate_random_number(list_for_random)
+        self.remove_punctuation(string)
+        self.generate_random_string(10)
+
+
+obj = Module()
+obj.call_functions()
--- helper/test_base.py
+++ helper/test_base.py
@@ -1,16 +1,60 @@
+import random
+import string
 
 
-class TestBase():
+class TestBase:
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
     def add_numbers(self, list_of_num):
-        print("----------")
-
         total = 0
         for ele in range(0, len(list_of_num)):
             total = total + list_of_num[ele]
-        print("Sum of numbers: ", total)
+        sum_of_numbers = round(total, 3)
+        print("Sum of numbers: ", sum_of_numbers)
 
-    def 
+    def subtract_numbers(self, list_of_num):
+        try:
+            res = list_of_num[1] - list_of_num[0]
+            sub = round(res, 3)
+            print("Difference of numbers:", sub)
+        except IndexError:
+            print("***Error: List index is out of range***")
+
+    def multiply_numbers(self, list_of_num):
+        res = 1
+        for ele in list_of_num:
+            res *= ele
+        product = round(res, 3)
+        print("Product of numbers: ", product)
+
+    def divide_numbers(self, list_of_num):
+        try:
+            result = list_of_num[1]/list_of_num[0]
+            quotient = round(result, 3)
+            print("Quotient of numbers: ", quotient)
+        except IndexError:
+            print("***Error: List index is out of range***")
+        except ZeroDivisionError:
+            print("***Error: Cannot divide number by zero***")
+
+    def generate_random_number(self, list_of_num):
+        try:
+            res = random.randint(list_of_num[0], list_of_num[1])
+            print("Random number: ", res)
+        except Exception as e:
+            print(e, "Exception occured")
+
+    def remove_punctuation(self, string):
+        punc = "{}:?><.(@!#$^%&+=)"
+        no_punct = ""
+        for char in string:
+            if char not in punc:
+                no_punct = no_punct + char
+        print("String without punctuation: ", no_punct)
+
+    def generate_random_string(self, string_length):
+        ran = ''.join(random.choices(string.ascii_uppercase + string.digits, k=string_length))
+        print("The randomly generated string is : " + str(ran))
+
